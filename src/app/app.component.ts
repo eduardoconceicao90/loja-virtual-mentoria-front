550b37fe0f94b10f5a40f5a288ab4325b8e7cf58
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-18 10:26:10 -0300

    Direcionamento automático Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from './services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor (private loginService: LoginService,
+               private router: Router){
+  }
+
+  ngOnInit(): void {
+    if(this.loginService.usuarioLogado()){
+      this.router.navigate(['home']);
+    }else{
+      this.router.navigate(['login']);
+    }
+  }
 }
