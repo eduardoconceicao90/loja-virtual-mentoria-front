d3ed05d039feba9644afd9b659e85dec11e54a3c
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-18 12:59:05 -0300

    Definindo ROLES para Rotas

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -3,5 +3,6 @@ import { inject } from '@angular/core';
 import { LoginService } from '../services/login.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  console.info(route.data)
   return inject(LoginService).usuarioLogado();
 };
