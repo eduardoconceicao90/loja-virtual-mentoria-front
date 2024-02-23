b1b64f0d11a0dd7af114c813ff5ac46c74ffee92
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-23 14:19:23 -0300

    Montando FormGroup

--- src/app/components/categoria-produto/categoria-produto.component.ts
+++ src/app/components/categoria-produto/categoria-produto.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CategoriaProduto } from 'src/app/model/categoria-produto';
 import { CategoriaProdutoService } from 'src/app/services/categoria-produto.service';
 
 @Component({
@@ -11,5 +12,25 @@ export class CategoriaProdutoComponent {
 
   constructor(private fb: FormBuilder,
               private categoriaProdutoService: CategoriaProdutoService) { }
+
+  /* Pegar dados do formulário */
+  categoriaProdutoForm = this.fb.group({
+    id:[],
+    nomeDesc: [null, Validators.required]
+  });
+
+  /* Transformar em objeto */
+  categoriaProdutoObjeto(): CategoriaProduto {
+    return {
+      id: this.categoriaProdutoForm.get('id')?.value!,
+      nomeDesc: this.categoriaProdutoForm.get('nomeDesc')?.value!,
+      empresa: this.categoriaProdutoForm.get('empresa')?.value!
+    }
+  }
+
+  cadastrarProdutoCategoria(){
+    const categoria = this.categoriaProdutoObjeto();
+    console.log(categoria)
+  }
 
 }
