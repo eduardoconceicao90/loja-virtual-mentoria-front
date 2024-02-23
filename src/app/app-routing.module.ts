1c49e75d2ea6300091e154f494ad03efb6b5ac67
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-23 15:16:45 -0300

    Atribuindo empresa ao user logado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './security/login/login.component';
-import { AppComponent } from './app.component';
 import { authGuard } from './security/guard/auth.guard';
 import { CategoriaProdutoComponent } from './components/categoria-produto/categoria-produto.component';
 
 const routes: Routes = [
   { path:'login', component: LoginComponent },
-  { path:'', component: AppComponent },
+  { path:'', redirectTo: 'home', pathMatch: 'full' },
   { path:'home', component: HomeComponent, canActivate: [authGuard], data: { role:['ROLE_USER', 'ROLE_ADMIN'] } },
   { path:'categoria-produto', component: CategoriaProdutoComponent, canActivate: [authGuard], data: { role:['ROLE_ADMIN'] } },
 ];
--- src/app/security/login.service.ts
+++ src/app/security/login.service.ts
@@ -24,6 +24,7 @@ export class LoginService {
         var jwt = JSON.parse(resJson);
         localStorage.setItem('Authorization', jwt.Authorization);
         localStorage.setItem('username', jwt.username);
+        localStorage.setItem('empresa', jwt.empresa);
         this.router.navigate(['home']);
       },
       error: (error) => {
